cbf05b080e198e031c32316b086d569db32f6684
unknown <gho@SELT10078.tobii.intra>
2016-02-19 15:13:58 +0100

    added C# app : DemoWpf

---
+++ DemoWpf/DemoWpf/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿//-----------------------------------------------------------------------
+// Copyright 2014 Tobii Technology AB. All rights reserved.
+//-----------------------------------------------------------------------
+
+
+namespace UserPresenceWpf
+{
+    using System.Windows;
+    using System;
+
+
+    using Tobii.EyeX.Framework;
+    using Tobii.EyeX.Client;
+    using Awesomium.Core;
+    using Awesomium.Windows.Controls;
+    using System.IO;
+
+    using System.Diagnostics;
+    using System.ComponentModel;
+
+    using System.Runtime.InteropServices;
+
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //webControl.Source = new Uri(new FileInfo(@"html/sensory-game/sensory-game.html").FullName);
+            //webControl.Source = new Uri(new FileInfo(@"Tobii.EyeX.Web/samples/Demos/JSDemo2.html").FullName);
+
+            //// porting web console to native console
+            //webControl.ConsoleMessage += (sender1, args) => Console.Out.WriteLine("[JS]{1}:{2} {0}", args.Message, args.Source, args.LineNumber);
+            //webControl.ShowContextMenu += (sender1, args) => args.Handled = true;
+
+            ActivateApp("iexplore");
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            ((MainWindowModel)DataContext).PropertyChanged += OnPropertyChanged;
+        }
+
+
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            MainWindowModel model = (MainWindowModel)DataContext;
+            //Console.WriteLine("A property has changed: " + e.PropertyName);
+            if (e.PropertyName == "IsUserDelayedPresent" && model.IsUserDelayedPresent)
+            {
+                Console.WriteLine("Before: Eyetracking mode:" + model.EyeXHost.EyeTrackingDeviceStatus);
+
+                model.EyeXHost.LaunchGuestCalibration(model.CalibrationHandler);
+
+                model.CancelDelayedTask();
+
+                Console.WriteLine("After: Eyetracking mode:" + model.EyeXHost.EyeTrackingDeviceStatus);
+
+                //ActivateApp("iexplore");
+            }
+        }
+
+
+        private void ActivateApp(string processName)
+        {
+            //test get other process
+            //Process[] localAll = Process.GetProcesses();
+            Process[] processes = Process.GetProcessesByName(processName);
+
+            // Activate the first application we find with this name
+
+            foreach (Process p in processes)
+            {
+                if (p.MainWindowTitle != "")
+                    SetForegroundWindow(p.MainWindowHandle);
+            }
+
+        }
+    }
+}
---
+++ DemoWpf/DemoWpf/MainWindowModel.cs
@@ -0,0 +1,293 @@
+﻿//-----------------------------------------------------------------------
+// Copyright 2014 Tobii Technology AB. All rights reserved.
+//-----------------------------------------------------------------------
+
+namespace UserPresenceWpf
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows;
+    using EyeXFramework;
+    using EyeXFramework.Wpf;
+    using Tobii.EyeX.Framework;
+    using Tobii.EyeX.Client;
+
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The MainWindowModel retrieves the UserPresence state from the WpfEyeXHost,
+    /// and sets up a listener for changes to the state. It exposes a property
+    /// ImageSource, which changes depending on the UserPresence state.
+    /// </summary>
+    public class MainWindowModel : INotifyPropertyChanged, IDisposable
+    {
+        private readonly WpfEyeXHost _eyeXHost;
+        private string _imageSource;
+        private bool _isUserPresent;
+        private bool _isUserDelayedPresent;
+        private CancellationTokenSource _delayedTaskTokenSource; 
+
+        private bool _isTrackingGaze;
+        private bool _isTrackingGazeSupported;
+
+        private AsyncData _calibrationAsyncData;
+        private bool _isCalibrating;
+
+
+        public MainWindowModel()
+        {
+            IsUserPresent = false;
+            IsTrackingGaze = false;
+            IsTrackingGazeSupported = true;
+
+            IsUserDelayedPresent = false;
+            _isCalibrating = false;
+            //_delayedTaskTokenSource = new CancellationTokenSource();  
+
+            // Create and start the WpfEyeXHost. Starting the host means
+            // that it will connect to the EyeX Engine and be ready to 
+            // start receiving events and get the current values of
+            // different engine states. In this sample we will be using
+            // the UserPresence engine state.
+            _eyeXHost = new WpfEyeXHost();
+
+            // Register an status-changed event listener for UserPresence.
+            // NOTE that the event listener must be unregistered too. This is taken care of in the Dispose(bool) method.
+            _eyeXHost.UserPresenceChanged += EyeXHost_UserPresenceChanged;
+            _eyeXHost.GazeTrackingChanged += EyeXHost_GazeTrackingChanged;
+
+            _eyeXHost.EyeTrackingDeviceStatusChanged += EyeXHost_EyeTrackingDeviceStatusChanged;
+
+            // Start the EyeX host.
+            _eyeXHost.Start();
+
+            // Wait until we're connected.
+            if (_eyeXHost.WaitUntilConnected(TimeSpan.FromSeconds(5)))
+            {
+                // Make sure the EyeX Engine version is equal to or greater than 1.4.
+                var engineVersion = _eyeXHost.GetEngineVersion().Result;
+                if (engineVersion.Major != 1 || engineVersion.Major == 1 && engineVersion.Minor < 4)
+                {
+                    IsTrackingGazeSupported = false;
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public WpfEyeXHost EyeXHost
+        {
+            get { return _eyeXHost; }
+        }
+
+        /// <summary>
+        /// A path to an image corresponding to the current UserPresence state.
+        /// </summary>
+        public string ImageSource
+        {
+            get { return _imageSource; }
+        }
+
+        /// <summary>
+        /// Gets whether or not the user is present.
+        /// </summary>
+        public bool IsUserPresent
+        {
+            get { return _isUserPresent; }
+            private set
+            {
+                if (_isUserPresent == value)
+                    return;
+
+                _isUserPresent = value;
+                _imageSource = _isUserPresent
+                    ? "/Images/present.png"
+                    : "/Images/not-present.png";
+
+                // Notify of properties that have changed.
+                OnPropertyChanged("IsUserPresent");
+                OnPropertyChanged("ImageSource");
+            }
+        }
+
+        /// <summary>
+        /// Gets whether or not the user is present.
+        /// </summary>
+        public bool IsUserDelayedPresent
+        {
+            get { return _isUserDelayedPresent; }
+            private set
+            {
+                if (_isUserDelayedPresent == value)
+                    return;
+
+                _isUserDelayedPresent = value;
+
+                OnPropertyChanged("IsUserDelayedPresent");
+            }
+
+        }
+
+        public void CancelDelayedTask()
+        {
+            _delayedTaskTokenSource.Cancel();
+            //_delayedTaskTokenSource.Dispose();  
+
+            //TODO: also handle non-cancel disposal
+        }
+
+
+        public void CalibrationHandler(AsyncData asyncData)
+        {
+            Console.WriteLine("Calibration handler");
+            _calibrationAsyncData = asyncData; //this async data doesn't seem to tell anything
+
+            _isCalibrating = true;
+        }
+
+        /// <summary>
+        /// Gets whether or not gaze is being tracked.
+        /// </summary>
+        public bool IsTrackingGaze
+        {
+            get { return _isTrackingGaze; }
+            private set
+            {
+                _isTrackingGaze = value;
+                OnPropertyChanged("IsTrackingGaze");
+            }
+        }
+
+        public bool IsTrackingGazeSupported
+        {
+            get { return _isTrackingGazeSupported; }
+            set
+            {
+                _isTrackingGazeSupported = value;
+                OnPropertyChanged("IsTrackingGazeSupported");
+                OnPropertyChanged("IsTrackingGazeNotSupported");
+            }
+        }
+
+        public bool IsTrackingGazeNotSupported
+        {
+            get { return !IsTrackingGazeSupported; }
+        }
+
+        /// <summary>
+        /// Cleans up any resources being used.
+        /// </summary>
+        public void Dispose()
+        {
+            _eyeXHost.UserPresenceChanged -= EyeXHost_UserPresenceChanged;
+            _eyeXHost.GazeTrackingChanged -= EyeXHost_GazeTrackingChanged;
+
+            _eyeXHost.EyeTrackingDeviceStatusChanged -= EyeXHost_EyeTrackingDeviceStatusChanged;
+            _eyeXHost.Dispose();
+
+            _delayedTaskTokenSource.Dispose();
+        }
+
+        private void EyeXHost_UserPresenceChanged(object sender, EngineStateValue<UserPresence> value)
+        {
+            //ignore any user presence change during calibration
+            if (_isCalibrating)
+                return;
+
+            //!!With the above checking, below isn't necesary
+            //check state and see if in calibration, then ignore this event
+            //EngineStateValue<EyeTrackingDeviceStatus> deviceState = _eyeXHost.EyeTrackingDeviceStatus;
+            //if (deviceState.IsValid && (deviceState.Value == EyeTrackingDeviceStatus.Configuring || deviceState.Value == EyeTrackingDeviceStatus.Initializing))
+            //    return;
+
+            bool isUserPresent = value.IsValid && value.Value == UserPresence.Present;
+
+            //add delay , only call when present for 2s
+            SetDelayedUserPresentAsync(isUserPresent);
+
+            // State-changed events are received on a background thread.
+            // But operations that affect the GUI must be executed on the main thread.
+            RunOnMainThread(() =>
+            {
+                Console.WriteLine("User Presenence changed: " + value.IsValid + " " + value.Value);
+                IsUserPresent = isUserPresent;
+            });
+        }
+
+        async Task SetDelayedUserPresentAsync(bool isPresent)
+        {
+            int delayMs = isPresent ? 2000 : 5000;
+            _delayedTaskTokenSource = new CancellationTokenSource();  
+
+            try
+            {
+                await Task.Delay(delayMs, _delayedTaskTokenSource.Token);
+                //if UserPresence stays the same after x sec, then set it 
+                if (IsUserPresent == isPresent)
+                {
+                    RunOnMainThread(() =>
+                    {
+                        Console.WriteLine("Delayed User Presenence changed: " + isPresent);
+                        IsUserDelayedPresent = isPresent;
+                    });
+                }
+            }
+            catch (TaskCanceledException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+        }
+
+        private void EyeXHost_GazeTrackingChanged(object sender, EngineStateValue<GazeTracking> value)
+        {
+            // State-changed events are received on a background thread.
+            // But operations that affect the GUI must be executed on the main thread.
+            RunOnMainThread(() =>
+            {
+                IsTrackingGaze = value.IsValid && value.Value == GazeTracking.GazeTracked;
+            });
+        }
+
+        private void EyeXHost_EyeTrackingDeviceStatusChanged(object sender, EngineStateValue<EyeTrackingDeviceStatus> value)
+        {
+            Console.WriteLine(" Tracking Status changed: " + value.IsValid + " " + value.Value);
+            //TEMP workaround to track when calibrating is done: first tracking deflag calibrating state
+            if (_isCalibrating && value.IsValid && value.Value == EyeTrackingDeviceStatus.Tracking)
+            {
+                _isCalibrating = false;
+                //Console.WriteLine(_calibrationAsyncData);
+            }
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        /// <summary>
+        /// Marshals the given operation to the UI thread.
+        /// </summary>
+        /// <param name="action">The operation to be performed.</param>
+        private static void RunOnMainThread(Action action)
+        {
+            if (Application.Current != null)
+            {
+                Application.Current.Dispatcher.BeginInvoke(action);
+            }
+        }
+    }
+}
