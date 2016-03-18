feda5dffc77b2dc28504b096288d95653d5e52e4
unknown <gho@SELT10078.tobii.intra>
2016-03-18 16:23:07 +0100

    [web] codes clean up!

--- eyex-web-games/app/components/Tobii.EyeX.Web/js/Web/EyeX/Foundation/Injection.js
+++
@@ -1,7 +0,0 @@
-﻿//EyeX.setLogLevelEnabled("debug", false);
-EyeX.ready(function (context) {
-    EyeX.dev.initialize(context);
-    //   EyeX.dev.showQueryBox();
-    EyeX.dom.enableDOMDecoration();
-    EyeX.dom.enableDOMScanning();
-});
